b619dae4682ebb24cf6455538b33f250b28be6c2
Egon <loliegon@gmail.com>
2022-03-05 19:46:48 +0100

    finished table endpoints

--- index.ts
+++ index.ts
@@ -21,6 +21,30 @@ const getUserbyId = db.prepare(`
 SELECT * FROM users WHERE id = (?);
 `);
 
+const getPosts = db.prepare(`
+SELECT * FROM posts;
+`);
+
+const getPostById = db.prepare(`
+SELECT * FROM posts WHERE id = (?);
+`);
+
+const getComments = db.prepare(`
+SELECT * FROM comments;
+`);
+
+const getCommentById = db.prepare(`
+SELECT * FROM comments WHERE id = (?);
+`);
+
+const getSubreddits = db.prepare(`
+SELECT * FROM subreddits
+`);
+
+const getSubredditById = db.prepare(`
+SELECT * FROM subreddits WHERE id = (?)
+`);
+
 app.get(`/users`, (req, res) => {
   const users = getUsers.all();
   res.send(users);
@@ -34,15 +58,54 @@ app.get(`/users/:id`, (req, res) => {
   } else res.status(404).send({ error: "Not found!" });
 });
 
-app.post(`/login`, (req, res) => {
-  const { email, password } = req.body;
+app.get(`/posts`, (req, res) => {
+  const posts = getPosts.all();
+  res.send(posts);
 });
 
-app.post(`/users`, (req, res) => {
-  const { name, email, password, displayName } = req.body;
-  const info = createUser.run(name, email, password, displayName);
-  res.send();
+app.get(`/posts/:id`, (req, res) => {
+  const id = req.params.id;
+  const post = getPostById.get(id);
+  if (post) {
+    res.send(post);
+  } else res.status(404).send({ error: "Not found!" });
 });
+
+app.get(`/comments`, (req, res) => {
+  const comments = getComments.all();
+  res.send(comments);
+});
+
+app.get(`/comments/:id`, (req, res) => {
+  const id = req.params.id;
+  const comment = getCommentById.get(id);
+  if (comment) {
+    res.send(comment);
+  } else res.status(404).send({ error: "Not found!" });
+});
+
+app.get(`/subreddits`, (req, res) => {
+  const subreddits = getSubreddits.all();
+  res.send(subreddits);
+});
+
+app.get(`/subreddits/:id`, (req, res) => {
+  const id = req.params.id;
+  const subreddit = getSubredditById.get(id);
+  if (subreddit) {
+    res.send(subreddit);
+  } else res.status(404).send({ error: "Not found!" });
+});
+
+// app.post(`/login`, (req, res) => {
+//   const { email, password } = req.body;
+// });
+
+// app.post(`/users`, (req, res) => {
+//   const { name, email, password, displayName } = req.body;
+//   const info = createUser.run(name, email, password, displayName);
+//   res.send();
+// });
 
 app.listen(4000, () => {
   console.log(`Running on http://localhost:4000`);
